83cd48da2e083fed8ca516a34a177d2d27a5d931
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-10-02 13:46:57 +0300

    add list of projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 let gulp = require("gulp");
+let gulpIgonre = require("gulp-ignore");
 let sass = require("gulp-sass");
 let browserSync = require("browser-sync");
 // let nunjucksRender = require("gulp-nunjucks-render");
 // let concat = require('gulp-concat');
 // let rename = require('gulp-rename');
 // let uglify = require('gulp-uglify');
+
+let condition = "./src/**/*";
 
 gulp.task("sass", function() {
   gulp
@@ -30,44 +33,40 @@ gulp.task("sass", function() {
 //     );
 // });
 
-gulp.task("index.html", function() {
-  return gulp.src("./src/index.html").pipe(gulp.dest("./docs"));
-});
+// gulp.task("index.html", function() {
+//   return gulp.src("./src/index.html").pipe(gulp.dest("./docs"));
+// });
 
-gulp.task("copy:img", function() {
-  return gulp.src("./src/img/**/*").pipe(gulp.dest("./docs/img"));
-});
+// gulp.task("copy:img", function() {
+//   return gulp.src("./src/img/**/*").pipe(gulp.dest("./docs/img"));
+// });
 
 gulp.task("serv", function() {
   browserSync.init({
     server: {
-      baseDir: "./docs"
+      baseDir: "./"
     },
     notify: false
   });
-  browserSync.watch("./docs/**/*", browserSync.reload);
+  browserSync.watch("./**/*", "!./src/**/*", browserSync.reload);
 });
 
-gulp.task("scripts", function() {
-  return (
-    gulp
-      .src("./src/js/**/*.js")
-      // .pipe(concat('scripts.js'))
-      .pipe(gulp.dest("./docs/scripts/"))
-    // .pipe(uglify())
-    // .pipe(rename('scripts.min.js'))
-  );
-});
+// gulp.task("scripts", function() {
+//   return (
+//     gulp
+//       .src("./src/js/**/*.js")
+//       // .pipe(concat('scripts.js'))
+//       .pipe(gulp.dest("./docs/scripts/"))
+//     // .pipe(uglify())
+//     // .pipe(rename('scripts.min.js'))
+//   );
+// });
 
-gulp.task(
-  "default",
-  ["index.html", "sass", "copy:img", "serv", "scripts"],
-  function() {
-    gulp.watch("./src/*.html", ["index.html"]);
-    gulp.watch("./src/scss/**/*.scss", ["sass"]);
-    // gulp.watch("./src/templates/**/*.html", ["nunjucks"]);
-    gulp.watch("./src/img/**/*", ["copy:img"]);
-    gulp.watch("./src/js/**/*.js", ["scripts"]);
-    gulp.watch("./docs/**/*", browserSync.reload);
-  }
-);
+gulp.task("default", ["sass", "serv"], function() {
+  // gulp.watch("./src/*.html", ["index.html"]);
+  gulp.watch("./src/scss/**/*.scss", ["sass"]);
+  // gulp.watch("./src/templates/**/*.html", ["nunjucks"]);
+  // gulp.watch("./src/img/**/*", ["copy:img"]);
+  // gulp.watch("./src/js/**/*.js", ["scripts"]);
+  gulp.watch("./**/*", "!./src/**/*", browserSync.reload);
+});
